0d1ccba1603521459b158ebf153347494245ea4c
Jonathan Wanderley <jonathan.wpc@gmail.com>
2023-05-01 18:33:46 -0300

    feat: implement POST /clientes

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import cors from "cors";
 import "dotenv/config";
 import "reflect-metadata";
 import "./database";
+import { routes } from "./routes";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(routes);
 
 app.listen(port, () => {
   `Running sucessfully in port ${port}`
