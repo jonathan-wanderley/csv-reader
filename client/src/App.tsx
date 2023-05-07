629bdc36bf3c8f7ace6c79df6d263143ec246a7e
Jonathan Wanderley <jonathan.wpc@gmail.com>
2023-05-07 15:52:56 -0300

    feat(client): add container and heading title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
+import { Box, Container, Heading } from '@chakra-ui/react'
 
 function App() {
 
   return (
     <>
-      
+      <Container maxW='2xl' bg='white' centerContent>
+        <Box padding='4' bg='white' color='black' maxW='full'>
+          
+          <Heading as='h1' size='2xl' >
+            Upload CSV to database
+          </Heading>
+
+        </Box>
+      </Container>
     </>
   )
 }
