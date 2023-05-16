8a3ac2d9ca5c6701eff277e5307350ccaac7e5d9
Jonathan Wanderley <jonathan.wpc@gmail.com>
2023-05-15 23:00:27 -0300

    feat: del Dockerfile and config project to use docker-compose only

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import "dotenv/config"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+     usePolling: true,
+    },
+    host: true, // Here
+    strictPort: true,
+    port: Number(process.env.PORT),
+  }
 })
